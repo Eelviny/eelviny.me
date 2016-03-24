6346077b9f0d798bad1fa2a3e69f18841cad78a9
Eelviny <elvinluff@gmail.com>
2016-03-24 15:52:04 +0000

    Add cache control headers to everything

--- upload.sh
+++ upload.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 JEKYLL_ENV=production jekyll build
-aws s3 sync static/ s3://eelviny/ --delete --storage-class REDUCED_REDUNDANCY
+aws s3 sync static/ s3://eelviny/ --delete --storage-class REDUCED_REDUNDANCY --cache-control max-age=86400
