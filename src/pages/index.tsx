ad9fd1b211b7c50879c794c096f2ea572e50573e
Elvin Luff <elvin@eelviny.me>
2025-04-16 16:48:27 +0200

    feat: try gatsby

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,11 +5,19 @@ import "normalize.css"
 
 const Home = (props: PageProps) => (
   <div>
-    <div className={styles.wrapper}>
+    <div className={styles.hero}>
       <div className={styles.main}>
         <h1 className="impact">
           Elvin Luff <span style={{ color: "white"}}>is a</span><br />Cloud Systems Engineer <span style={{ color: "white"}}>in</span> Amsterdam.
         </h1>
+      </div>
+    </div>
+    <div className={styles.homeBody}>
+      <div>
+        <p>Testing 123!</p>
+      </div>
+      <div>
+        <p>another test</p>
       </div>
     </div>
   </div>
